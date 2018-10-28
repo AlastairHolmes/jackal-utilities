7b214a9b83c3e574431b7523182d9a4bcb897cec
Alastair Holmes <holmes.studio@outlook.com>
2018-10-28 12:45:24 +0000

    bitwise doxygen documentation

--- src/jkutil/bitwise.h
+++ src/jkutil/bitwise.h
@@ -23,58 +23,6 @@ namespace jkutil
 		10000 (16) | 10000
 		00001 (1)  | 11111
 	*/
-	constexpr uint32_t all_highest_bits(uint32_t p_bits);
-	constexpr uint64_t all_highest_bits(uint64_t p_bits);
-
-	/*!
-		@brief Returns a value with only the highest value bit in \p p_bits set.
-
-		@details Examples in 5 bits (For simplicity):
-		Input      | Output
-		---------- | -------
-		01100 (12) | 01000
-		11111 (31) | 10000
-		00000 (0)  | 00000
-		00111 (7)  | 00100
-	*/
-	constexpr uint32_t set_highest_bit(uint32_t p_bits);
-	constexpr uint64_t set_highest_bit(uint64_t p_bits);
-
-	/*!
-		@brief Returns a value with all the bits, that are less or equally significant to the lowest set bit in \p p_bits, set.
-
-		@details Examples in 5 bits (For simplicity):
-		Input      | Output
-		---------- | -------
-		00010 (2)  | 00011
-		00000 (0)  | 00000
-		10000 (16) | 11111
-		00001 (1)  | 00001
-	*/
-	constexpr uint32_t all_lowest_bits(uint32_t p_bits);
-	constexpr uint64_t all_lowest_bits(uint64_t p_bits);
-
-	/*!
-		@brief Returns a value with only the lowest value bit in \p p_bits set. 
-
-		@details Examples in 5 bits (For simplicity):
-		Input      | Output
-		---------- | -------
-		01100 (12) | 00100
-		11111 (31) | 00001
-		00000 (0)  | 00000
-		00111 (7)  | 00001
-	*/
-	constexpr uint32_t set_lowest_bit(uint32_t p_bits);
-	constexpr uint64_t set_lowest_bit(uint64_t p_bits);
-
-	/*!
-		@brief Rounds up to the nearest power of two
-		@details For any number more than '2^31' (32 bit) or '2^63' (64 bit) returns '2^31' or '2^63' respectively.
-	*/
-	constexpr uint32_t ceil_pow2(uint32_t p_number);
-	constexpr uint64_t ceil_pow2(uint64_t p_number);
-
 	constexpr uint32_t all_highest_bits(uint32_t p_bits)
 	{
 		uint32_t mask = p_bits;
@@ -98,6 +46,17 @@ namespace jkutil
 		return mask;
 	}
 
+	/*!
+		@brief Returns a value with only the highest value bit in \p p_bits set.
+
+		@details Examples in 5 bits (For simplicity):
+		Input      | Output
+		---------- | -------
+		01100 (12) | 01000
+		11111 (31) | 10000
+		00000 (0)  | 00000
+		00111 (7)  | 00100
+	*/
 	constexpr uint32_t set_highest_bit(uint32_t p_bits)
 	{
 		uint32_t temp = all_highest_bits(p_bits);
@@ -110,6 +69,17 @@ namespace jkutil
 		return temp ^ (temp >> 1);
 	}
 
+	/*!
+		@brief Returns a value with all the bits, that are less or equally significant to the lowest set bit in \p p_bits, set.
+
+		@details Examples in 5 bits (For simplicity):
+		Input      | Output
+		---------- | -------
+		00010 (2)  | 00011
+		00000 (0)  | 00000
+		10000 (16) | 11111
+		00001 (1)  | 00001
+	*/
 	constexpr uint32_t all_lowest_bits(uint32_t p_bits)
 	{
 		uint32_t mask = p_bits;
@@ -133,6 +103,17 @@ namespace jkutil
 		return mask;
 	}
 
+	/*!
+		@brief Returns a value with only the lowest value bit in \p p_bits set. 
+
+		@details Examples in 5 bits (For simplicity):
+		Input      | Output
+		---------- | -------
+		01100 (12) | 00100
+		11111 (31) | 00001
+		00000 (0)  | 00000
+		00111 (7)  | 00001
+	*/
 	constexpr uint32_t set_lowest_bit(uint32_t p_bits)
 	{
 		uint32_t temp = all_lowest_bits(p_bits);
@@ -145,6 +126,10 @@ namespace jkutil
 		return temp ^ (temp << 1);
 	}
 
+	/*!
+		@brief Rounds up to the nearest power of two
+		@details For any number more than '2^31' (32 bit) or '2^63' (64 bit) returns '2^31' or '2^63' respectively.
+	*/
 	constexpr uint32_t ceil_pow2(uint32_t p_number)
 	{
 		if (p_number < 2)
