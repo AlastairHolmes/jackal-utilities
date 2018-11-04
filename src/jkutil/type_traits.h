0fd2f8b211e8e4e57e8900066b64350c232ff1a4
Alastair Holmes <holmes.studio@outlook.com>
2018-11-04 14:36:31 +0000

    type_traits additions
    all_unique and all_same

--- src/jkutil/type_traits.h
+++ src/jkutil/type_traits.h
@@ -12,8 +12,33 @@
 namespace jkutil
 {
 
-	//is_storeable_allocator
-	//is_allocator
+	template <bool... Bs>
+	struct variadic_and : public std::true_type
+	{};
+
+	template <bool B1, bool... Bs>
+	struct variadic_and<B1, Bs...> : public std::bool_constant<B1 && variadic_and<Bs...>::value>
+	{};
+
+	template <class... Ts>
+	struct all_unique : public std::true_type {};
+
+	template <class T, class... Ts>
+	struct all_unique<T, Ts...> : public std::bool_constant<
+		variadic_and<(!std::is_same<T, Ts>::value)...>::value && all_unique<Ts...>::value>
+	{};
+
+	template <class... comparedTypes>
+	struct all_same : public std::false_type {};
+
+	template <class comparedType>
+	struct all_same<comparedType> : public std::true_type {};
+
+	template <class comparedType1, class comparedType2, class... comparedTypes>
+	struct all_same<comparedType1, comparedType2, comparedTypes...> : public std::conditional<
+		std::is_same<comparedType1, comparedType2>::value,
+			typename all_same<comparedType2, comparedTypes...>::type,
+			std::false_type> {};
 
 }
 
