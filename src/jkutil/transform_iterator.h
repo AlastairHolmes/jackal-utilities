5ce71ab6fb54e93f7359d84dcf8175e8fd230492
Alastair Holmes <holmes.studio@outlook.com>
2018-12-07 15:27:26 +0000

    make_transform_range

--- src/jkutil/transform_iterator.h
+++ src/jkutil/transform_iterator.h
@@ -7,6 +7,7 @@
 #ifndef JKUTIL_TRANSFORM_ITERATOR_H
 #define JKUTIL_TRANSFORM_ITERATOR_H
 
+#include <jkutil\utility.h>
 #include <iterator>
 #include <type_traits>
 
@@ -89,7 +90,6 @@ namespace jkutil
 		transformCallableType m_callable;
 		iteratorType m_iterator;
 
-
 	};
 
 	template <class iteratorType, class transformCallableType>
@@ -102,6 +102,14 @@ namespace jkutil
 	transform_iterator<std::decay_t<iteratorType>, std::decay_t<transformCallableType>> make_transform_iterator(iteratorType&& p_iterator, transformCallableType&& p_callable)
 	{
 		return transform_iterator<std::decay_t<iteratorType>, std::decay_t<transformCallableType>>(std::forward<iteratorType>(p_iterator), std::forward<transformCallableType>(p_callable));
+	}
+
+	template <class rangeType, class transformCallableType>
+	decltype(auto) make_transform_range(rangeType&& p_range, transformCallableType&& p_callable)
+	{
+		return make_iterator_range(
+			make_transform_iterator(p_range.begin(),std::forward<transformCallableType>(p_callable)),
+			make_transform_iterator(p_range.end(),std::forward<transformCallableType>(p_callable)));
 	}
 
 }
